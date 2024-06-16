d6d4f31ef3194e0aeb1fd8f761e1716f6d773226
krylovis <kis.226@yandex.ru>
2024-06-16 08:28:57 +0300

    fix

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
   plugins: [handlebars()],
-  root: resolve(__dirname, 'static'),
+  root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
   },
