324041651f72ae711dbcf92f682c9cf73a2e344f
krylovis <kis.226@yandex.ru>
2024-06-16 11:00:13 +0300

    fix app add main

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import Handlebars from 'handlebars';
 import * as Components from './components/index';
 import * as Pages from './pages/index';
+
+const pages = {
+  'login': [ Pages.LoginPage ],
+};
+
+Object.entries(Components).forEach(([ name, component ]) => {
+  Handlebars.registerPartial(name, component);
+});
+
+const app = document.querySelector('#app');
+
+function navigate(page) {
+  const [ source, args ] = pages[page];
+  const handlebarsFunct = Handlebars.compile(source);
+  
+  app.innerHTML = handlebarsFunct(args);
+}
+
