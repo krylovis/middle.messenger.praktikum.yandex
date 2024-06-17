6a21923c42091372561dfa3ba474b1470574ff7a
krylovis <kis.226@yandex.ru>
2024-06-17 20:00:33 +0300

    add nav-link

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { FormContainer } from './form-container';
 export { Button } from './button';
 export { Input } from './input';
 export { InputField } from './input-field';
+export { NavLink } from './nav-link';
