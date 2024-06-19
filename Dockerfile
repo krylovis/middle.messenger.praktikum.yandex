ac2fee6b2d2c90bfa8498043aa6c9ab09e2ff53d
Krylov Ivan <krylovis@mchs.ru>
2024-06-19 16:24:45 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY package*.json .
 RUN npm install --legacy-peer-deps
 COPY . .
 RUN npm run build
-# RUN npm run server
+# RUN npm run start
 FROM nginx:stable-alpine as production
 COPY --from=development /usr/src/app/dist /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/conf.d/default.conf
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
 const __dirname = new URL('.', import.meta.url).pathname;
 
-app.use(express.static(`${__dirname}/dist`));
+// app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   console.log('dist/index.html');
-  res.sendFile(`${__dirname}/dist/index.html`);
+  // res.sendFile(`${__dirname}/dist/index.html`);
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 app.listen(PORT, () => {
