7f3a8cc03b02a82ffded794056e1dbf41507903c
Krylov Ivan <krylovis@mchs.ru>
2024-06-29 16:50:21 +0300

    fix server

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 import express from 'express';
-import path from 'path';
+import { engine } from 'express-handlebars';
 
 const app = express();
 const PORT = 3000;
-const __dirname = new URL('.', import.meta.url).pathname;
 
-// app.use(express.static(`${__dirname}/dist`));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './src/components/pages');
 
-app.get('*', (req, res) => {
-  console.log('dist/index.html');
-  // res.sendFile(`${__dirname}/dist/index.html`);
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('/', (req, res) => {
+    console.log('res');
+    res.render('main-page');
 });
 
 app.listen(PORT, () => {
