49af1da7a1f09cbdfba5d148122e33e78350dba2
krylovis <kis.226@yandex.ru>
2024-07-07 15:10:17 +0300

    add EventBus

---
+++ src/utils/EventBus/index.ts
@@ -0,0 +1,34 @@
+export default class EventBus {
+  readonly listeners: Record<string, Function[]>;
+
+  constructor() {
+    this.listeners = {};
+  }
+
+  on(event: string, callback: () => void) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(callback);
+  }
+
+  off(event: string, callback: () => void): void {
+		if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event] = this.listeners[event]
+    .filter((listener) => listener !== callback);
+  }
+
+	emit(event: string, ...args: unknown[]): void {
+    if (!this.listeners[event]) {
+      throw new Error(`Нет события: ${event}`);
+    }
+
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
+}
