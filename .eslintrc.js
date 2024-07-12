ef668bab284adbf6b5eb526fd1bcb28eadf036fd
Krylov Ivan <krylovis@mchs.ru>
2024-07-12 17:27:46 +0300

    add linter

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+export default [{
+  rules: {
+    "@html-eslint/no-duplicate-attrs": "error"
+  }
+}];
--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,24 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import html from "@html-eslint/eslint-plugin";
+import htmlParser from "@html-eslint/parser";
 
 
 export default [
+  {
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    plugins: {
+      "@html-eslint": html,
+    },
+    settings: {
+      "html/indent": "+2",
+    },
+    languageOptions: {
+      parser: htmlParser,
+    },
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts}"]
   },
