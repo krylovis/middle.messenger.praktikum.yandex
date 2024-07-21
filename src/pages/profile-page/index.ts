9199cd1b8dc151c22f24df851daf5d9201b374bf
krylovis <kis.226@yandex.ru>
2024-07-21 19:02:08 +0300

    add inputs

--- src/pages/profile-page/index.ts
+++ src/pages/profile-page/index.ts
@@ -3,7 +3,6 @@ import avatar from '../../../static/images/captain-jack-sparrow.jpg';
 import {
   Input,
   NavLink,
-  InputError,
   InputField,
   ProfileAvatar,
   ProfileForm
@@ -26,6 +25,120 @@ const profileAvatar = new ProfileAvatar({
   name: profile.first_name
 });
 
+// email input
+const profileEmail = new Input({
+  id: "profileEmail",
+  name: "email",
+  type: "email",
+  attr: {
+    class: "input_type_profile",
+    value: profile.email,
+    disabled: true,
+  }
+});
+
+const profileEmailField = new InputField ({
+  Input: profileEmail,
+  id: "profileEmail",
+  label: "Почта",
+  attr: { class: "input-field_type_profile" }
+});
+
+// login input
+const profileLogin = new Input({
+  id: "profileLogin",
+  name: "login",
+  type: "text",
+  attr: {
+    class: "input_type_profile",
+    value: profile.login,
+    disabled: true,
+  }
+});
+
+const profileLoginField = new InputField ({
+  Input: profileLogin,
+  id: "profileLogin",
+  label: "Логин",
+  attr: { class: "input-field_type_profile" }
+});
+
+// first name input
+const profileFirstName = new Input({
+  id: "profileFirstName",
+  name: "first_name",
+  type: "text",
+  attr: {
+    class: "input_type_profile",
+    value: profile.first_name,
+    disabled: true,
+  }
+});
+
+const profileFirstNameField = new InputField ({
+  Input: profileFirstName,
+  id: "profileFirstName",
+  label: "Имя",
+  attr: { class: "input-field_type_profile" }
+});
+
+// second name input
+const profileSecondName = new Input({
+  id: "profileSecondName",
+  name: "second_name",
+  type: "text",
+  attr: {
+    class: "input_type_profile",
+    value: profile.second_name,
+    disabled: true,
+  }
+});
+
+const profileSecondNameField = new InputField ({
+  Input: profileSecondName,
+  id: "profileSecondName",
+  label: "Фамилия",
+  attr: { class: "input-field_type_profile" }
+});
+
+// display name input
+const profileDisplayName = new Input({
+  id: "profileDisplayName",
+  name: "display_name",
+  type: "text",
+  attr: {
+    class: "input_type_profile",
+    value: profile.display_name,
+    disabled: true,
+  }
+});
+
+const profileDisplayNameField = new InputField ({
+  Input: profileDisplayName,
+  id: "profileDisplayName",
+  label: "Имя&nbsp;в&nbsp;чате",
+  attr: { class: "input-field_type_profile" }
+});
+
+// phone input
+const profilePhone = new Input({
+  id: "profilePhone",
+  name: "phone",
+  type: "phone",
+  attr: {
+    class: "input_type_profile",
+    value: profile.phone,
+    disabled: true,
+  }
+});
+
+const profilePhoneField = new InputField ({
+  Input: profilePhone,
+  id: "profilePhone",
+  label: "Телефон",
+  attr: { class: "input-field_type_profile" }
+});
+
 const navLinkEdit = new NavLink ({
   toPage: "edit-profile-page",
   text: "Изменить данные",
@@ -45,7 +158,14 @@ const navLinkLogout = new NavLink ({
 });
 
 const profileForm = new ProfileForm({
-  lists: [],
+  lists: [
+    profileEmailField,
+    profileLoginField,
+    profileFirstNameField,
+    profileSecondNameField,
+    profileDisplayNameField,
+    profilePhoneField,
+  ],
 
   NavLinkEdit: navLinkEdit,
   NavLinkPassword: navLinkPassword,
