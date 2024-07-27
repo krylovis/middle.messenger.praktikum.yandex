e29ec2f8bee6f7beacca68b38466bcb4352a61a8
Krylov Ivan <krylovis@mchs.ru>
2024-07-27 12:10:26 +0300

    add navigate

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,63 @@
-// import Handlebars from 'handlebars';
-// import * as Components from '@/components/index';
-// import * as Pages from '@/pages/index';
-import PopupWithForm from '@/components/popup/PopupWithForm';
+import * as Pages from '@/pages/index';
 import render from '@/utils/render';
-import { profilePage } from '@/pages';
 
-render('#app', profilePage);
+import PopupWithForm from '@/components/popup/PopupWithForm';
 
-// enum EPages {
-//   LoginPage = 'login',
-//   RegisterPage = 'register',
-//   ProfilePage = 'profile-page',
-//   EditProfilePage = 'edit-profile-page',
-//   ChangePasswordPage = 'change-password-page',
-//   MainPage = 'main-page',
-//   ErrorPage = 'error-page',
-// }
+enum EPages {
+  LoginPage = 'login',
+  RegisterPage = 'register',
+  ProfilePage = 'profile-page',
+  EditProfilePage = 'edit-profile-page',
+  ChangePasswordPage = 'change-password-page',
+  MainPage = 'main-page',
+  // ErrorPage = 'error-page',
+}
 
-// const pages: any = {
-//   [EPages.LoginPage]: [Pages.LoginPage],
-//   [EPages.RegisterPage]: [Pages.RegisterPage],
-//   [EPages.ProfilePage]: [Pages.ProfilePage],
-//   [EPages.EditProfilePage]: [Pages.EditProfilePage],
-//   [EPages.ChangePasswordPage]: [Pages.ChangePasswordPage],
-//   [EPages.MainPage]: [Pages.MainPage],
-//   [EPages.ErrorPage]: [Pages.ErrorPage],
-// };
+const pages: any = {
+  [EPages.LoginPage]: [Pages.loginPage],
+  [EPages.RegisterPage]: [Pages.registerPage],
+  [EPages.ProfilePage]: [Pages.profilePage],
+  [EPages.EditProfilePage]: [Pages.editProfilePage],
+  [EPages.ChangePasswordPage]: [Pages.changePasswordPage],
+  [EPages.MainPage]: [Pages.mainPage],
+  // [EPages.ErrorPage]: [Pages.ErrorPage],
+};
 
-// Object.entries(Components).forEach(([name, component]) => {
-//   Handlebars.registerPartial(name, component);
-// });
+const app: any = document.querySelector('#app');
 
-// const app: any = document.querySelector('#app');
+function navigate(page: string, args?: { error: string, text: string }): void {
+  const [source] = pages[page];
+  render('#app', source);
+}
 
-// function navigate(page: string, args?: { error: string, text: string }): void {
-//   const [source] = pages[page];
-//   const handlebarsFunct = Handlebars.compile(source);
-
-//   app.innerHTML = handlebarsFunct(args);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   switch (window.location.pathname) {
-//     case '/':
-//       navigate('main-page');
-//       break
-//     case '/login':
-//       navigate('login');
-//       break
-//     case '/register':
-//       navigate('register');
-//       break
-//     case '/profile':
-//       navigate('profile-page');
-//       break
-//     case '/edit-profile':
-//       navigate('edit-profile-page');
-//       break
-//     case '/404':
-//       navigate('error-page', { error: '404', text: 'Не туда попали' });
-//       break
-//     case '/500':
-//       navigate('error-page', { error: '500', text: 'Мы уже фиксим' });
-//       break
-//     case '/change-password':
-//       navigate('change-password-page');
-//       break
-//   }
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  switch (window.location.pathname) {
+    case '/':
+      navigate('main-page');
+      break
+    case '/login':
+      navigate('login');
+      break
+    case '/register':
+      navigate('register');
+      break
+    case '/profile':
+      navigate('profile-page');
+      break
+    case '/edit-profile':
+      navigate('edit-profile-page');
+      break
+    case '/404':
+      navigate('error-page', { error: '404', text: 'Не туда попали' });
+      break
+    case '/500':
+      navigate('error-page', { error: '500', text: 'Мы уже фиксим' });
+      break
+    case '/change-password':
+      navigate('change-password-page');
+      break
+  }
+})
 
 const profileEditAvatarButton = document.querySelector('.profile-avatar__edit-button');
 // const menuButton = document.querySelector('.chat-footer__menu');
