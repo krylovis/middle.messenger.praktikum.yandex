6d83cf7e1010a9b3221707942c3b73a3087ea244
Krylov Ivan <krylovis@mchs.ru>
2024-07-27 10:09:51 +0300

    fix

--- src/pages/main-page/template.ts
+++ src/pages/main-page/template.ts
@@ -3,12 +3,12 @@ import './main-page.scss';
 export const mainPageTemplate = `
 <section class="main-page">
   <div class="main-page__chat-list-container">
-    {{ ChatList }}
+    {{{ ChatList }}}
   </div>
 
   <div class="main-page__content main-page__content_type_chat">
-    {{ ChatHeader }}
-    {{ ChatContent }}
-    {{ ChatFooter }}
+    {{{ ChatHeader }}}
+    {{{ ChatContent }}}
+    {{{ ChatFooter }}}
   </div>
 </section>`;
