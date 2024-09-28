ce1c532df0df95f0f4d779d16cc4adf8627537cf
krylovis <kis.226@yandex.ru>
2024-09-28 11:25:15 +0300

    add _request

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,28 +1,70 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { BASE_URL, HEADERS} from '@/utils/constants'
 
-interface TParams {
-  baseUrl: string,
-  headers: Record<string, string>,
-}
-
 enum METHODS {
 	GET = 'GET',
 	POST = 'POST',
 	PUT = 'PUT',
-	DELETE = 'DELETE'
+	DELETE = 'DELETE',
+}
+interface IParams {
+  baseUrl: string,
+  headers: Record<string, string>,
+}
+interface IReqParams {
+  method: METHODS,
+  path?: string,
+  timeout?: number,
+  data?: FormData | string,
 }
 
 class Api {
   _baseUrl;
   _headers;
 
-  constructor({ baseUrl, headers }: TParams) {
+  constructor({ baseUrl, headers }: IParams) {
     this._baseUrl = baseUrl;
     this._headers = headers;
   }
 
-  // const request = new XMLHttpRequest();
+  private _request({ method, path = '', timeout, data }: IReqParams) {
+    return new Promise((resolve, reject) => {
+      const req = new XMLHttpRequest();
+      req.open(method, `${this._baseUrl}${path}`);
+
+      // req.withCredentials = true;
+      req.responseType = 'json';
+      if(timeout) {
+        req.timeout = timeout;
+      }
+
+      if(this._headers.length) {
+        for (const key in this._headers) {
+          req.setRequestHeader(key, this._headers[key]);
+        }
+      }
+
+      req.onload = () => {
+        if ([200, 201].includes(req.status)) {
+          resolve(req.response);
+        } else {
+          reject(new Error(`Ошибка сервера: ${req.statusText}`));
+        }
+      };
+
+      req.onerror = () => {
+        reject(new Error(`Ошибка соединения: ${req.status}`));
+      };
+
+      if (method === METHODS.GET || method === METHODS.PUT && data) {
+				req.send(data);
+			} else {
+				req.send();
+			}
+
+      req.send();
+    })
+  }
 }
 
 export const api = new Api({
