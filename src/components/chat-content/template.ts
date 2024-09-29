13b4988e78f43ca804af77e4cc09fe8b5f6e7d93
krylovis <kis.226@yandex.ru>
2024-09-29 11:57:42 +0300

    fix chat-content template

--- src/components/chat-content/template.ts
+++ src/components/chat-content/template.ts
@@ -1,3 +1,14 @@
 import './chat-content.scss';
 
-export const chatContentTemplate = `<div class="chat-content"></div>`;
+export const chatContentTemplate = `
+<div class="chat-content">
+  {{#if lists}}
+    <ul class="chat-content__message-list">
+      {{{ lists }}}
+    </ul>
+  {{ else }}
+    <p class="chat-content__empty-text">
+      Выберите чат чтобы отправить сообщение
+    </p>
+  {{/if}}
+</div>`;
