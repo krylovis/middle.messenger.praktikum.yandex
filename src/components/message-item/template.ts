9219643fa09e2d53c27d429572136f8e8749085f
Krylov Ivan <krylovis@mchs.ru>
2024-09-30 11:17:52 +0300

    add message info

--- src/components/message-item/template.ts
+++ src/components/message-item/template.ts
@@ -3,4 +3,8 @@ import './message-item.scss';
 export const messageItemTemplate = `
 <li class="message-item">
   {{ message }}
+  <div class="message-item__info">
+    <span class="message-item__time">{{ sentedAt }}</span>
+    {{#if owner}}<span class="message-item__icon"></span>{{/if}}
+  </div>
 </li>`;
