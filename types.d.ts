5e3c00307f7bc79413caa70c8cc14cd3a5bfc69a
krylovis <kis.226@yandex.ru>
2024-10-04 09:14:41 +0300

    fix

--- types.d.ts
+++ types.d.ts
@@ -11,6 +11,8 @@ declare module '*.hbs' {
 declare module "*.png"
 declare module "*.jpg"
 declare module '*.scss';
+declare module "*.module.css";
+declare module "*.module.scss";
 
 declare module 'uuid';
 declare module '@/utils/FormValidator';
