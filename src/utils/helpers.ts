45eeb3462994d18f094456b29f148555dc93d188
krylovis <kis.226@yandex.ru>
2024-11-09 19:59:45 +0300

    add isEqual

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -6,6 +6,50 @@ export function isEqualValue<T>(lhs: T, rhs: T): boolean {
   return lhs === rhs;
 }
 
+function isArray(value: unknown): value is [] {
+  return Array.isArray(value);
+}
+
+type PlainObject<T = unknown> = {
+  [k in string]: T;
+};
+
+function isPlainObject(value: unknown): value is PlainObject {
+  return typeof value === 'object'
+    && value !== null
+    && value.constructor === Object
+    && Object.prototype.toString.call(value) === '[object Object]';
+}
+
+function isArrayOrObject(value: unknown): value is ([] | PlainObject) {
+  return isPlainObject(value) || isArray(value);
+}
+
+export function isEqual<T>(lhs: T, rhs: T) {
+  if (isPlainObject(lhs) && isPlainObject(rhs)) {
+    if (Object.keys(lhs).length !== Object.keys(rhs).length) {
+      return false;
+    }
+
+    for (const [key, value] of Object.entries(lhs)) {
+      const rightValue = rhs[key];
+      if (isArrayOrObject(value) && isArrayOrObject(rightValue)) {
+        if (isEqual(value, rightValue)) {
+          continue;
+        }
+
+        return false;
+      }
+
+      if (value !== rightValue) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
 export function merge(lhs: Indexed, rhs: Indexed): Indexed {
   for (const p in rhs) {
     if (!rhs.hasOwnProperty(p)) {
