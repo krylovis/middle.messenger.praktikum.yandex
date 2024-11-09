48d72ba319be40f00614e1a3c690622cdaff0c77
krylovis <kis.226@yandex.ru>
2024-11-09 11:37:52 +0300

    add store

---
+++ src/utils/Store/index.ts
@@ -0,0 +1,42 @@
+import EventBus from "@/utils/EventBus";
+import { set } from "@/utils/helpers";
+
+export enum StoreEvents {
+  Updated = 'updated',
+}
+
+interface IUser {
+  "id": number,
+  "first_name": string,
+  "second_name": string,
+  "display_name": string,
+  "phone": string,
+  "login": string,
+  "avatar": string,
+  "email": string
+}
+
+export interface IState {
+  currentUser?: IUser;
+}
+
+class Store extends EventBus {
+  private state: IState = {};
+
+  constructor() {
+    super();
+    this.state;
+  }
+
+  public set(path: string, value: unknown) {
+    set(this.state, path, value);
+
+    this.emit(StoreEvents.Updated);
+  };
+
+  public getState() {
+    return this.state;
+  }
+}
+
+export default new Store();
