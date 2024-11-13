5359fbafc05035cb624296535f3491aac5a72bd3
krylovis <kis.226@yandex.ru>
2024-11-13 09:24:32 +0300

    fix profileAvatarTemplate

--- src/components/profile-avatar/template.ts
+++ src/components/profile-avatar/template.ts
@@ -5,7 +5,11 @@ export const profileAvatarTemplate = `
 <div class="profile-avatar">
   {{#if isEditAvatar}}
   <button class="profile-avatar__edit-button" type="button" aria-label="Редактировать аватар профиля">
-    <img src={{ avatar }} alt="Аватар" class="profile-avatar__avatar profile-avatar__avatar_type_edit" />
+    {{#if avatar}}
+      <img src={{ avatar }} alt="Аватар" class="profile-avatar__avatar" />
+    {{else}}
+      <img src="${defaultAvatar}" alt="Аватар" class="profile-avatar__avatar" />
+    {{/if}}
   </button>
   {{else}}
     {{#if avatar}}
@@ -13,7 +17,7 @@ export const profileAvatarTemplate = `
     {{else}}
       <img src="${defaultAvatar}" alt="Аватар" class="profile-avatar__avatar" />
     {{/if}}
-  {{/if}}
 
-  <p class="profile-avatar__name">{{ name }}</p>
+    <p class="profile-avatar__name">{{ name }}</p>
+  {{/if}}
 </div>`;
