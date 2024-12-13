d903a0e86a7cc5440ba5942921950c3454508928
krylovis <kis.226@yandex.ru>
2024-12-13 16:02:48 +0300

    add chat-list__header

--- src/components/chat-list/template.ts
+++ src/components/chat-list/template.ts
@@ -2,11 +2,12 @@ import './chat-list.scss';
 
 export const chatListTemplate = `
 <div class="chat-list">
-  {{{ NavLink }}}
+  <div class="chat-list__header">
+    {{{ NavLink }}}
+    <input class="chat-list__search-input" type="search" id="search-input" name="search-input" placeholder="Поиск">
+    {{{ AddChatBtn }}}
+  </div>
 
-  <input class="chat-list__search-input" type="search" id="search-input" name="search-input" placeholder="Поиск">
-
-  {{{ AddChatBtn }}}
   <ul class="chat-list__content">
     {{{ lists }}}
   </ul>
