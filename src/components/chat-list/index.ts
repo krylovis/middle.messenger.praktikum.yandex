d30ec43b689519402f40420d2bcfc74fd1a0f36a
krylovis <kis.226@yandex.ru>
2024-12-13 15:20:01 +0300

    add connectWithChats

--- src/components/chat-list/index.ts
+++ src/components/chat-list/index.ts
@@ -1,8 +1,23 @@
 import Block, { IData } from '@/utils/Block';
+import { connectWithChats } from '@/utils/connects';
 import { chatListTemplate } from './template';
 
-export default class ChatList extends Block<IData> {
+class ChatList extends Block<IData> {
+  constructor(props: IData) {
+    super({
+      ...props,
+    })
+  }
+
+  public componentBeforeMount(): void {
+    if (this.props.chatsList) {
+      //
+    }
+  }
+
   render() {
     return chatListTemplate;
   }
 }
+
+export default connectWithChats(ChatList)
--- src/utils/connects/index.ts
+++ src/utils/connects/index.ts
@@ -2,10 +2,12 @@ import connect from './hoc';
 import connectWithUser from './connectWithUser';
 import connectWithAvatar from './connectWithAvatar';
 import connectWithPopup from './connectWithPopup';
+import connectWithChats from './connectWithChats';
 
 export {
   connect,
   connectWithUser,
   connectWithAvatar,
   connectWithPopup,
+  connectWithChats,
 };
