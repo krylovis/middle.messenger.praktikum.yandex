56e87b2ed2a41172f2f3d2ce7dcc6ff3da47f6c0
krylovis <kis.226@yandex.ru>
2024-12-13 15:18:07 +0300

    add chatsList to store

--- src/utils/Store/index.ts
+++ src/utils/Store/index.ts
@@ -17,7 +17,28 @@ interface IUser {
   "email": string
 }
 
+interface IChats {
+  "id": number,
+  "title": string,
+  "avatar": string,
+  "unread_count": number,
+  "created_by": number,
+  "last_message": {
+    "user": {
+      "first_name": string,
+      "second_name": string,
+      "avatar": string,
+      "email": string,
+      "login": string,
+      "phone": string
+    },
+    "time": string,
+    "content": string
+  }
+}
+
 export interface IState {
+  chatsList?: IChats[];
   currentUser?: IUser;
   isPopupOpen?: boolean;
 }
@@ -40,7 +61,7 @@ class Store extends EventBus {
     return this.state;
   }
 
-  public getState(key: string): IUser| boolean | null {
+  public getState(key: string): IUser | boolean | null | IChats[] {
     const state = this.state[key as keyof IState];
 
     if (state) {
