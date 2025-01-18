5e8224ecdbaa408d41346cb33c2340fe51d27565
krylovis <kis.226@yandex.ru>
2025-01-18 12:46:25 +0300

    fix chatHeaderTemplate

--- src/components/chat-header/template.ts
+++ src/components/chat-header/template.ts
@@ -3,7 +3,14 @@ import './chat-header.scss';
 export const chatHeaderTemplate = `
 <div class="chat-header">
   {{{ Avatar }}}
-  <span class="chat-header__name">Опоссум</span>
+
+  <span class="chat-header__name">
+    {{#if chatName}}
+      {{chatName}}
+    {{else}}
+      ...
+    {{/if}}
+  </span>
 
   <div class="chat-header__menu-container">
     {{{ DropdownMenu }}}
